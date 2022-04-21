60f8d07cff7cbcf118129a8bdcb6294e8019491b
drewkuznetsov <8150309@gmail.com>
2022-04-21 16:44:38 +0300

    Initial Commit

---
+++ RozaEinema/RozaEinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RozaEinemaApp.swift
+//  RozaEinema
+//
+//  Created by Андрей Кузнецов on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct RozaEinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
